c9f97176d2df6805fcc0607117da78cf33edeba9
Danil-Kot <py.prog@mail.ru>
2022-04-14 14:30:37 +0800

    fix мелких багов в login.php | main.php

--- views/main.php
+++ views/main.php
@@ -4,12 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=H, initial-scale=1.0">
-    <title>Document</title>
+    <title>site</title> // потом придумаем новое
 </head>
 <body>
-    <?php
-   
-    ?>
+
     <h1> ГЛАВНАЯ СТРАНИЦА САЙТА</h1>
 </body>
 </html>
